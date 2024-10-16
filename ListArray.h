1075b190eb4378d6fce992002ba2730ca5842b2f
Javier Veyrat <146321233+jVeyZ@users.noreply.github.com>
2024-10-16 09:52:31 +0200

    Añadida implementación de la clase ListArray

--- ListArray.h
+++ ListArray.h
@@ -1,4 +1,6 @@
+#include <iostream>
 #include <ostream>
+#include <stdexcept>
 #include "List.h"
 
 template <typename T> 
@@ -8,21 +10,131 @@ class ListArray : public List<T> {
       T* arr;
       int max;
       int n;
-      static const int MINSIZE; 
+      static const int MINSIZE = 2; 
 
-      void resize(int new_size);
+      void resize(int new_size){
+       T* newArr = new T[new_size];
+        for (int i = 0; i < n; ++i) {
+            newArr[i] = arr[i];
+        }
+        delete[] arr;
+        arr = newArr;
+        max = new_size;
+      };
 
 
     public:
+      ListArray(){
+        arr = new T[MINSIZE];
+        max = MINSIZE;
+        n = 0;
+      }
+
+      ~ListArray(){
+        delete arr;
+      }
+
       void insert(int pos, T e) override {
+        try {
+          if (pos <= n && pos >= 0) {
+            if (n == max) {
+              resize(max * 2);
+            }
+            for (int i = n; i > pos; --i) {
+              arr[i] = arr[i - 1];
+            }
+            arr[pos] = e;
+            ++n;
+          } else {
+            throw std::out_of_range("Position out of range");
+          }
+        } catch (const std::out_of_range& e) {
+          std::cout << e.what() << std::endl;
+        }
 
       }
+
       void append(T e) override{
-
+        insert(n, e);
       }
+
       void prepend(T e) override{
+        insert(0, e); 
+      }
+
+      T remove(int pos) override{
+        int val;
+        try {
+          if (pos < n && pos >= 0) {
+            val = arr[pos];
+            for (int i = pos; i < n - 1; ++i) {
+              arr[i] = arr[i + 1];
+            }
+            --n;
+          } else {
+            throw std::out_of_range("");
+          }
+        } catch (std::out_of_range&) {
+          std::cout << "out_of_range";
+        }
+        return val;
+      }
+
+      T get(int pos) override{
+         try {
+          if (pos <= max && pos >= 0) {
+          return arr[pos];
+          } 
+          else {
+          throw std::out_of_range("");
+          }
+        }
+        catch (std::out_of_range()) {
+          std::cout << "out_of_range";
+        }
 
       }
-      
+
+      int search(T e) override{
+        int i = 0;
+        for (const T& element: arr) {
+          if (element == e) {
+          return i;}
+          i++;
+        }
+        return -1;
+      }
+
+      bool empty() override{
+        if (size() == 0) {
+        return true;
+        }
+      }
+
+      int size() override{
+        return n; 
+      }
+
+      T operator[](int pos){
+        if (pos >= 0 && pos < n) {
+          return arr[pos];
+        }
+        else {
+        throw std::out_of_range("");
+        }
+      }
+
+      friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list) {
+        out << "[";
+        for (int i = 0; i < list.n; ++i) {
+          out << list.arr[i];
+          if (i < list.n - 1) {
+        out << ", ";
+          }
+        }
+        out << "]";
+        return out;
+      }
+
 
 };
